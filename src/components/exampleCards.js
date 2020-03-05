a9cfb651a0bf9181381b5dfdda5e33af20b0b387
Otzoy <Stephanie_Otzoy@Dellteam.com>
2020-03-05 06:29:26 -0600

    work in progress

--- src/components/exampleCards.js
+++ src/components/exampleCards.js
@@ -1,16 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function ExampleCards(props){
+
+    const [checkered, setCheckered] = useState(false)
+    const onClicker = (e) =>{
+        setCheckered(e.target.value);
+    }
     return(
     <div>
     <form className={props.done === true ? 'done' : null} done={props.done} id={props.id} onClick={props.removeHandler}>
     <h1 id={props.id}>{props.title}</h1>
-    <input type="radio" name={props.name} id={`yes ${props.group + props.id}`} value="yes"/>
-    <label for={`yes ${props.group + props.id}`}>yes</label>
-    <input type="radio" name={props.name} id={`no ${props.group + props.id}`} value="no"/>
-    <label for={`no ${props.group + props.id}`}>no</label>
-    <input type="radio" name={props.name} id={`sometimes ${props.group + props.id}`} value="sometimes"/>
-    <label for={`sometimes ${props.group + props.id}`}>sometimes</label>
+    <label>
+    <input
+    type="radio"
+    name={props.name}
+    id={`yes ${props.group + props.id}`}
+    value="yes"
+    onClick={onClicker}
+    checked={checkered === "yes"}/>
+    yes</label>
+    <label>
+    <input
+    type="radio"
+    name={props.name}
+    id={`no ${props.group + props.id}`}
+    value="no"
+    onClick={onClicker}
+    checked={checkered === "no"}/>
+    no</label>
+    <label>
+    <input
+    type="radio"
+    name={props.name}
+    id={`sometimes ${props.group + props.id}`}
+    value="sometimes"
+    onClick={onClicker}
+    checked={checkered === "sometimes"}/>
+    sometimes</label>
     </form>
     </div>
     )}
